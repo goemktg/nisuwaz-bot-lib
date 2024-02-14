0749041cb7fb004446fc3717e8099b597b150bde
goemktg <samktg52@gmail.com>
2024-02-14 14:23:52 +0900

    refactor: 커맨드 처리 리팩터링

---
+++ commands-handler.js
@@ -0,0 +1,64 @@
+const { REST, Routes, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const dotenv = require('dotenv');
+
+// 환경변수 불러옴
+dotenv.config();
+
+// 모듈로 사용될 경우
+if (require.main !== module)
+	module.exports = handleCommands;
+// 독립적으로 실행될 경우
+else 
+	handleCommands('deploy');
+
+async function handleCommands(type) {
+	const commands = [];
+	const outputCommands = new Collection();
+	const commandsPath = path.join(__dirname, '../commands');
+	// commands 풀더에 위치한 모든 .js 파일을 불러옴
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	
+	// 배포 전에 배포 무시할 명령어들을 가져옴
+	const ignoreCommands = JSON.parse(process.env.IGNORE_COMMANDS).list;
+
+	// 배포를 위해 모든 명령어 각각의 SlashCommandBuilder#toJSON() 데이터 결과를 가져옴
+	for (const file of commandFiles) {
+		const command = require(`${commandsPath}/${file}`);
+
+		// 무시할 명령어 목록에 해당 명령아가 있으면 스킵
+		if (ignoreCommands.includes(command.data.name))
+			continue;
+		
+		commands.push(command.data.toJSON());
+	
+		outputCommands.set(command.data.name, command);
+	}
+	// REST 모듈 instance 생성 및 준비
+	const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+	
+	// init 모드일시 배포하지 않고 명령어 목록 리턴함
+	if (type === 'init')
+		return outputCommands;
+
+	// 명령어 배포
+	try {
+		console.log(`Started deleting ALL application (/) commands.`);
+
+		const data = await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
+		.then(() => console.log('Successfully deleted all application commands.'))
+		.then(() => {
+			console.log(`Started deploying ${commands.length} application (/) commands.`);
+
+			return rest.put(
+				Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DEPLOY_TARGET_GUILD_ID),
+				{ body: commands },
+			);
+		});
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+}
