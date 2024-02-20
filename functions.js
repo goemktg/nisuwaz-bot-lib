c616fd5b5686a368bbce44a3f46be0f90dfa7382
goemktg <samktg52@gmail.com>
2024-02-20 18:43:13 +0000

    refactor: loadEnvironmentVariables 함수 추가 및 코드 스타일 수정 refactor: loadEnvironmentVariables 함수 추가 및 exports 업데이트 style: discord.js 스타일 가이드에 맞게 코드 수정

--- functions.js
+++ functions.js
@@ -1,87 +1,98 @@
+function loadEnvironmentVariables() {
+	try {
+		const dotenv = require('dotenv');
+		dotenv.config();
+	}
+	catch (ex) {
+		console.log('.env not found. using docker environment variable');
+	}
+}
+
+// FIXME: 더 깔끔한 로직으로 변경 필요
 async function checkRecruitStage(channel, recruitString) {
-    // Create message pointer
-    let message = await channel.messages
-    .fetch({ limit: 1 })
-    .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
-    // 만약 첫번째 항목이 찾는 항목일 경우 즉시 리턴
-    if (message.content == recruitString) return [false, message];
+	// 메시지 포인터 생성
+	let message = await channel.messages
+		.fetch({ limit: 1 })
+		.then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+	// 만약 첫번째 항목이 찾는 항목일 경우 즉시 리턴
+	if (message.content == recruitString) return [false, message];
 
-    while (message) {
-        const messagePage = await channel.messages.fetch({ limit: 100, before: message.id });
+	while (message) {
+		const messagePage = await channel.messages.fetch({ limit: 100, before: message.id });
 
-        for (const msg of messagePage) {
-            //console.log(msg[1].content);
-            if (msg[1].author.id == 1091569359794737242 && msg[1].content == recruitString) {
-                return [false, msg[1]];
-            }
-        }
+		for (const msg of messagePage) {
+			if (msg[1].author.id == 1091569359794737242n && msg[1].content == recruitString) {
+				return [false, msg[1]];
+			}
+		}
 
-        // Update our message pointer to be the last message on the page of messages
-        message = messagePage.size > 0 ? messagePage.at(messagePage.size - 1) : null;
-    }
+		// 메시지 포인터를 페이지의 마지막 메시지로 업데이트
+		message = messagePage.size > 0 ? messagePage.at(messagePage.size - 1) : null;
+	}
 
-    return [true];
+	return [true];
 }
 
 async function getRecruitInfo(channel, recruitStringArr) {
-    let returnObject = {};
+	const returnObject = {};
 
-    // Create message pointer
-    let message = await channel.messages
-    .fetch({ limit: 1 })
-    .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+	// Create message pointer
+	let message = await channel.messages
+		.fetch({ limit: 1 })
+		.then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
 
-    while (message) {
-        const messagePage = await channel.messages.fetch({ limit: 100, before: message.id });
+	while (message) {
+		const messagePage = await channel.messages.fetch({ limit: 100, before: message.id });
 
-        for (const msg of messagePage) {
-            const recruitStringArrIndex = recruitStringArr.indexOf(msg[1].content);
+		for (const msg of messagePage) {
+			const recruitStringArrIndex = recruitStringArr.indexOf(msg[1].content);
 
-            if (msg[1].author.id == 1091569359794737242 && recruitStringArrIndex !== -1) {
-                recruitStringArr.splice(recruitStringArrIndex, 1);
-                returnObject[msg[1].content] = msg[1];
-            }
+			if (msg[1].author.id == 1091569359794737242n && recruitStringArrIndex !== -1) {
+				recruitStringArr.splice(recruitStringArrIndex, 1);
+				returnObject[msg[1].content] = msg[1];
+			}
 
-            if (recruitStringArr.length === 0)
-                return returnObject;
-        }
+			if (recruitStringArr.length === 0) {
+				return returnObject;
+			}
+		}
 
-        // Update our message pointer to be the last message on the page of messages
-        message = messagePage.size > 0 ? messagePage.at(messagePage.size - 1) : null;
-    }
+		// Update our message pointer to be the last message on the page of messages
+		message = messagePage.size > 0 ? messagePage.at(messagePage.size - 1) : null;
+	}
 }
 
 function makeMessageLink(message) {
-    return `https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`;
+	return `https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`;
 }
 
 function isBanTarget(charInfoByType, recruiterRoleID) {
-    const {banData, banDataStringMapping} = require('../data.js');
+	const { banData, banDataStringMapping } = require('../data.js');
 
-    let returnBoolean = false;
-    let returnString = '';
+	let returnBoolean = false;
+	let returnString = '';
 
-    for (const type in banData) {
-        const currentData = banData[type];
-        Object.entries(currentData).some(prop => {
-            const set = new Set(prop[1]);
+	for (const type in banData) {
+		const currentData = banData[type];
+		Object.entries(currentData).some(prop => {
+			const set = new Set(prop[1]);
 
-            charInfoByType[prop[0]].forEach((item) => {
+			charInfoByType[prop[0]].forEach((item) => {
 
-                // 입갤밴 목록에 있음
-                if (set.has(item)) {
-                    if (!returnBoolean) {
-                        returnBoolean = true;
-                        returnString = `<@&${recruiterRoleID}> 주의! 현재 리크룻 중인 캐릭터 **${charInfoByType['char'][0]}**은(는) 밴 리스트에 등재되어 있습니다. 반드시 참고하시고 리크룻 바랍니다.\`\`\`밴 리스트는 다음과 같습니다:`;
-                    }
-                    returnString += `\n기록 중 ${item} ${banDataStringMapping[prop[0]]} ${banDataStringMapping[type]} 대상입니다`;
-                }
-            });
-        });
-    }
+				// 입갤밴 목록에 있음
+				if (set.has(item)) {
+					if (!returnBoolean) {
+						returnBoolean = true;
+						returnString = `<@&${recruiterRoleID}> 주의! 현재 리크룻 중인 캐릭터 **${charInfoByType['char'][0]}**은(는) 밴 리스트에 등재되어 있습니다. 반드시 참고하시고 리크룻 바랍니다.\`\`\`밴 리스트는 다음과 같습니다:`;
+					}
+					returnString += `\n기록 중 ${item} ${banDataStringMapping[prop[0]]} ${banDataStringMapping[type]} 대상입니다`;
+				}
+			});
+		});
+	}
 
-    return [returnBoolean, returnString+'```'];
+	return [returnBoolean, returnString + '```'];
 }
 
 // Export the function
-module.exports = { checkRecruitStage, makeMessageLink, getRecruitInfo, isBanTarget };
+module.exports = { checkRecruitStage, makeMessageLink, getRecruitInfo, isBanTarget, loadEnvironmentVariables };
