d756d1c9855522557834b38d6c0c4c66cc308a6c
goemktg <samktg52@gmail.com>
2024-02-24 13:13:52 +0900

    deploy 제대로 실행되지 않는 문제 수정 fix: deploy 시 발생하는 오류 수정 style: 필요 없는 주석 삭제

--- commands-handler.js
+++ commands-handler.js
@@ -3,34 +3,22 @@ const { loadEnvironmentVariables } = require('./functions.js');
 const fs = require('node:fs');
 const path = require('node:path');
 
-// 모듈로 사용될 경우
-if (require.main !== module) {module.exports = commandsHandler;}
-// 독립적으로 실행될 경우
-else {
-	const commandsHandler = new commandsHandler();
-	commandsHandler.depolyCommands();
-}
-
 class commandsHandler {
 	getCommands() {
 		const commands = [];
 		const outputCommands = new Collection();
 		const commandsPath = path.join(__dirname, '../commands');
-		// commands 풀더에 위치한 모든 .js 파일을 불러옴
+
 		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-		// 배포 전에 배포 무시할 명령어들을 가져옴
 		const ignoreCommands = JSON.parse(process.env.IGNORE_COMMANDS).list;
 
-		// 배포를 위해 모든 명령어 각각의 SlashCommandBuilder#toJSON() 데이터 결과를 가져옴
 		for (const file of commandFiles) {
 			const command = require(`${commandsPath}/${file}`);
 
-			// 무시할 명령어 목록에 해당 명령아가 있으면 스킵
 			if (ignoreCommands.includes(command.data.name)) {continue;}
 
 			commands.push(command.data.toJSON());
-
 			outputCommands.set(command.data.name, command);
 		}
 
@@ -38,7 +26,6 @@ class commandsHandler {
 	}
 
 	async depolyCommands() {
-		loadEnvironmentVariables();
 		const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 		const { commands } = this.getCommands();
 
@@ -84,4 +71,14 @@ class commandsHandler {
 			}
 		}
 	}
+}
+
+// 모듈로 사용될 경우
+if (require.main !== module) {module.exports = commandsHandler;}
+// 독립적으로 실행될 경우
+else {
+	loadEnvironmentVariables();
+
+	const commandHandler = new commandsHandler();
+	commandHandler.depolyCommands();
 }
