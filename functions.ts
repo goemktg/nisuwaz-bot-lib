698fcccd4ac7ba4876c2f5cdb2a5bc92537c7b30
goemktg <samktg52@gmail.com>
2024-02-29 04:23:07 +0900

    feat: 뉴비 롤 반영 라이브러리화

--- functions.ts
+++ functions.ts
@@ -1,4 +1,5 @@
 import { GuildAuditLogsEntry, Guild } from 'discord.js';
+import { DiscordRole } from './types';
 
 /**
  * .env 파일 또는 Docker 환경 변수에서 환경 변수를 로드합니다.
@@ -31,4 +32,25 @@ export async function getAuditTargetNickname(auditLog: GuildAuditLogsEntry, guil
 	}
 
 	return member.nickname;
+}
+
+/**
+ * auditLogsEntry 에서의 뉴비 롤 권한 변경을 반영합니다.
+ *
+ * @param {GuildAuditLogsEntry} auditLog - 길드 auditLogsEntry.
+ * @param {string} nickname - 유저의 닉네임.
+ * @param {(a: string) => void} addFunction - 롤 추가시 호출할 함수.
+ * @param {(a: string) => void} removeFunction - 롤 제거시 호출할 함수.
+ */
+export function reflectNewbieRoleChange(auditLog: GuildAuditLogsEntry, nickname: string, addFunction: (a: string) => void, removeFunction: (a: string) => void) {
+	const watchRoleIds = ['1210191232756621383', '1210112780141600818'];
+
+	for (const change of auditLog.changes) {
+		for (const newRole of (change.new as DiscordRole[])) {
+			if (!watchRoleIds.includes(newRole.id)) continue;
+
+			if (change.key === '$add') {addFunction(nickname);}
+			else if (change.key === '$remove') {removeFunction(nickname);}
+		}
+	}
 }
