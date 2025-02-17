65be8daaf7521ced5b070ba0461a8aeabea96e31
goemktg <samktg52@gmail.com>
2025-02-17 07:01:37 +0000

    fix: add checks for postgresql environment variables

--- classes/DatabaseHandler.ts
+++ classes/DatabaseHandler.ts
@@ -4,6 +4,15 @@ export class DatabaseHandler {
   private client: Pool;
 
   constructor() {
+    if (process.env.POSTGRES_HOST === undefined)
+      throw new Error("POSTGRES_HOST is not defined in environment variables.");
+    if (process.env.POSTGRES_USER === undefined)
+      throw new Error("POSTGRES_USER is not defined in environment variables.");
+    if (process.env.POSTGRES_PW === undefined)
+      throw new Error("POSTGRES_PW is not defined in environment variables.");
+    if (process.env.POSTGRES_DB === undefined)
+      throw new Error("POSTGRES_DB is not defined in environment variables.");
+
     this.client = new Pool({
       host: process.env.POSTGRES_HOST,
       user: process.env.POSTGRES_USER,
